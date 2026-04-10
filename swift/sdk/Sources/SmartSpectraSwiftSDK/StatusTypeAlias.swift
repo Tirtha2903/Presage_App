018017ec923dae2d3ab29a8054354172785b8a8e
Srijan Dey <dey.srijan005@gmail.com>
2026-04-11 04:08:39 +0530

    Deleted all file

--- swift/sdk/Sources/SmartSpectraSwiftSDK/StatusTypeAlias.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-// Preserve the public type name used by SDK consumers while
-// backing it with the Swift Protobuf-generated enum.
-public typealias StatusCode = Presage_Physiology_StatusCode
-
